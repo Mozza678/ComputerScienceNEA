cd33dcc01ec6af9879a43dd4468dbb1cf9f3521e
Mozza678 <mozzasworld@gmail.com>
2025-11-12 20:35:33 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Title");
+
+    while (window.isOpen()) {
+        while (auto event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+        window.clear(sf::Color(64,64,64));
+        window.display();
+    }
+
+    return 0;
+}
